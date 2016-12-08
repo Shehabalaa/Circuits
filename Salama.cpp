dfca0c92d8e48d2f7a4c1941304cf73ac7132df6
Your Name <salama@gmail.com>
2016-12-08 19:07:19 +0200

    msg

--- Salama.cpp
+++ Salama.cpp
@@ -1,2 +1,7 @@
 // thevinin or norton source 
 #include "Header.h"
+#include "Header.h" 
+double getVth(vector<Node> Nodes, float interesting_element)
+{
+	for (unsigned int i = 0; i< Nodes.size(); i++)
+}
